648f7ee9654cb20c364e88af19afad3de8b52830
Anna Kulkova <lasska98@yandex.ru>
2022-01-13 16:26:19 +0300

    WIP: add chart

---
+++ src/screens/Chart/styles.js
@@ -0,0 +1,10 @@
+import { styled } from 'baseui';
+
+export const Container = styled('div', () => {
+  return {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  };
+});
